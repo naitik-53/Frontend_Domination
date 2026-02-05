10dd435ee94b9c4ff2d91b8856b60e8401960cad
Naitik Kumar <naitikkumar6654@gmail.com>
2026-02-05 13:36:14 +0530

    Gsap Basics

---
+++ Gsap/Gsap Basics/script.js
@@ -0,0 +1,31 @@
+var tl = gsap.timeline();
+
+gsap.to("#box",{
+    x:1000,
+    rotate:360,
+    backgroundColor:"blue",
+    druation:2,
+    delay:2
+    
+})
+
+gsap.to("#box1",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    druation:3,
+    delay:2
+    
+})
+gsap.to("#box2",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    druation:3,
+})
+gsap.to("#box3",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    druation:3,
+})
