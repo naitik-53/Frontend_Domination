dfae253f02fc3641d9c00fa85c2d419bc5cf092d
Naitik Kumar <naitikkumar6654@gmail.com>
2026-02-06 11:37:56 +0530

    Responsive Part Done

--- Gsap/Practice 1/script.js
+++ Gsap/Practice 1/script.js
@@ -1,24 +1,55 @@
-var tl = gsap.timeline()
+var mm = gsap.matchMedia()
 
-tl.from("#nav h3", {
-    y: -50,
-    opacity: 0,
-    delay: 0.4,
-    duration:0.7,
-    stagger:0.3
+mm.add("(max-width: 768px)", function () {
+    var tl = gsap.timeline()
+
+    tl.from("#nav h3", {
+        y: -20,
+        opacity: 0,
+        delay: 0.2,
+        duration: 0.5,
+        stagger: 0.15
+    })
+
+    tl.from("#main h1", {
+        x: -120,
+        opacity: 0,
+        duration: 0.6,
+        stagger: 0.2
+    })
+
+    tl.from("img", {
+        x: 40,
+        rotate: 20,
+        opacity: 0,
+        duration: 0.4,
+        stagger: 0.25
+    })
 })
 
-tl.from("#main h1",{
-    x:-500,
-    opacity:0,
-    duration:0.8,
-    stagger:0.4
-})
+mm.add("(min-width: 769px)", function () {
+    var tl = gsap.timeline()
 
-tl.from("img",{
-    x:100,
-    rotate:45,
-    opacity:0,
-    duration:0.5,
-    stagger:0.5
+    tl.from("#nav h3", {
+        y: -50,
+        opacity: 0,
+        delay: 0.4,
+        duration: 0.7,
+        stagger: 0.3
+    })
+
+    tl.from("#main h1", {
+        x: -500,
+        opacity: 0,
+        duration: 0.8,
+        stagger: 0.4
+    })
+
+    tl.from("img", {
+        x: 100,
+        rotate: 45,
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.5
+    })
 })
