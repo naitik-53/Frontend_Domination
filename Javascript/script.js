708a970139fcdfcea3446b7c625d65208d596276
Naitik Kumar <naitikkumar6654@gmail.com>
2026-02-07 21:00:02 +0530

    javascript basics

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,9 @@
 let h1 = document.createElement("h1");
 h1.textContent = "Hello I Am Javascript";
 document.body.appendChild(h1);
+
+
+//learning console.error and console.warning
+
+console.error("This is an error message")
+console.warning("This is an warning message")
