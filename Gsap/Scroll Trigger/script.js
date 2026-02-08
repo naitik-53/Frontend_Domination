50e10a870c76b5ab2249cec6a4185ea698b9c5aa
Naitik Kumar <naitikkumar6654@gmail.com>
2026-02-08 09:30:57 +0530

    Gsap Scroll Trigger

---
+++ Gsap/Scroll Trigger/script.js
@@ -0,0 +1,27 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("#page1 .circle",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:720
+})
+gsap.from("#page2 .circle",{
+    scale:0,
+    duration:2,
+    rotate:720,
+    scrollTrigger:{
+        trigger:"#page2 .circle",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        end:"top 30%",
+        scrub:5
+    }
+})
+gsap.from("#page3 .circle",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:720
+})
