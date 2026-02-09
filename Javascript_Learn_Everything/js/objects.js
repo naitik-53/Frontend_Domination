93dfcf1c3cc7b3420e1a59cc36d26f5ad1a7102a
Naitik Kumar <naitikkumar6654@gmail.com>
2026-02-09 08:47:19 +0530

    JavaScript Everything

---
+++ Javascript_Learn_Everything/js/objects.js
@@ -0,0 +1,51 @@
+// objects
+
+// what
+
+// object ek tareeka hai jisse ki hum ek identity ki details ko ek saath rakh skte hai
+
+// object = ek bande ka kai saara data
+// array = kai logo ka data
+
+var obj = {
+  name: "lucky",
+  age: 23,
+  work: "developer",
+  address: "Kalindi Vihar,Agra",
+};
+
+var obj2 = {
+    name: "Naitik",
+    age: 16,
+    address: "kalimandir",
+};
+
+// var obj2 = new Object();
+
+// console.log(obj, obj2);
+
+// console.log(obj);
+
+console.log(obj.name);
+console.log(obj.work);
+
+var battery = {
+  company: "canon",
+  price: 1200,
+  for: "camera",
+  isWorking: true,
+};
+
+console.log(battery.price);
+
+// console.log(console);  // console object
+
+// how
+
+// {}
+
+// when & why
+
+// jab bhi apko ek hi element ki bhut saari details store krni ho
+
+// to save a lot of details all together at one place we use object
